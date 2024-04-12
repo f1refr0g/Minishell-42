aacd683a7bbd66a3e2d02ac1c2072bba105a38f7
Olivier Drolet St Cyr <oldrolet@c3r8p5.42quebec.com>
2024-04-12 00:05:05 -0400

    12 April 00:05:05

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -1,5 +1,17 @@
 #include "../minishell.h"
 
+void	set_mini_no_wrds(void)
+{
+	t_mini	*mini;
+	int		no_wrds;
+
+	no_wrds = 0;
+	mini = get_data();
+	while (mini->cmds[no_wrds])
+		no_wrds++;
+	mini->no_wrds = no_wrds;
+}
+
 char	**parse_mini_cmds(void)
 {
 	int		cmd_index;
@@ -8,5 +20,21 @@ char	**parse_mini_cmds(void)
 	t_mini	*mini;
 
 	mini = get_data();
-
+	set_mini_no_wrds();
+	new_cmds = ft_calloc(mini->no_wrds + 1, sizeof(char *));
+	if (!new_cmds)
+		return (NULL);
+	cmd_nb = 0;
+	cmd_index = -1;
+	while (mini->cmds[++cmd_index])
+	{
+		if (contain_cash(mini->cmds[cmd_index]) != 0
+			|| (mini->cmds[cmd_index][0] == '$' && mini->cmds[cmd_index][1]
+			&& mini->cmds[cmd_index][1] != '$'))
+			new_cmds[cmd_nb] = (ft_dollar_switch(mini->cmds[cmd_index]));
+		else
+			new_cmds[cmd_nb] = ft_strdup(mini->cmds[cmd_index]);
+		cmd_nb++;
+	}
+	return (new_cmds);
 }
--- srcs/quote.c
+++ srcs/quote.c
@@ -30,15 +30,23 @@ bool	valid_quote(char *str, int len, int quote)
 }
 
 //Check if input contain a dollars sign
-bool	contain_cash(char *str)
+int	contain_cash(char *input)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] && str[i] != '\0')
+	if (!input || !input[0])
+		return (0);
+	while (input[i])
 	{
-		if (str[i] == '$')
-			return (1);
+		if (input[i] == '$')
+		{
+			while (input[i + 1] && input[i + 1] == '$')
+				i++;
+			if (!input[i + 1])
+				return (0);
+			return (i);
+		}
 		i++;
 	}
 	return (0);
