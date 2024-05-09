3f250ebc38051e96c782d24ccb46cc23b968be1e
Olivier Drolet St Cyr <oldrolet@c3r12p6.42quebec.com>
2024-05-09 13:41:09 -0400

    09 May 13:41:09

--- srcs/token.c
+++ srcs/token.c
@@ -21,7 +21,6 @@ char	***ft_commandarray(t_data *data)
 		i++;
 	}
 	cmd_array[i] = NULL;
-	// print_cmd_array(cmd_array);
 	return (cmd_array);
 }
 
@@ -42,96 +41,10 @@ int	ft_parse(t_mini	*mini)
 	releaser(mini->cmds);
 	if (!tokeniser(mini))
 		return (0);
-	releaser(mini->new_cmds);//ici
+	releaser(mini->new_cmds);
 	if (!strncmp(mini->tokens->cmd[0], "exit", 5))
 		return (ft_exit(mini->tokens->cmd));
 	return (1);
-}
-
-//NE PAS POUBLIER LE DOLLAR SIGNE DANS MALLOC
-//Add a space before and after each redirection
-char	*ft_expend(t_data *data)
-{
-	int		i;
-	int		j;
-	char	*newstr;
-
-	i = 0;
-	j = 0;
-	newstr = ft_calloc(sizeof(ft_strlen(data->prompt)
-				+ ft_count(data->prompt) + 1), 100);
-	if (!newstr)
-	{
-		free(newstr);
-		// return NULL;
-		ft_error(MALLOC_ERROR);
-	}
-	while (data->prompt[i] != '\0')
-	{
-		if (data->prompt[i] == '|')
-		{
-			if (data->prompt[i + 1] == '|'
-				&& valid_quote(data->prompt, i + 1, QUOTE) == 0)
-			{
-				printf("Invalid syntax near token '||'\n");
-				break ;
-			}
-			newstr[j] = ' ';
-			j++;
-			newstr[j] = '|';
-			j++;
-			newstr[j] = ' ';
-		}
-		else if (data->prompt[i] == '>')
-		{
-			if (data->prompt[i + 1] != '>')
-			{
-				newstr[j] = ' ';
-				j++;
-				newstr[j] = '>';
-				j++;
-				newstr[j] = ' ';
-			}
-			else if (data->prompt[i + 1] == '>')
-			{
-				newstr[j] = ' ';
-				j++;
-				newstr[j] = '>';
-				j++;
-				newstr[j] = '>';
-				j++;
-				newstr[j] = ' ';
-				i += 1;
-			}
-		}
-		else if (data->prompt[i] == '<')
-		{
-			if (data->prompt[i + 1] != '<')
-			{
-				newstr[j] = ' ';
-				j++;
-				newstr[j] = '<';
-				j++;
-				newstr[j] = ' ';
-			}
-			else if (data->prompt[i + 1] == '<')
-			{
-				newstr[j] = ' ';
-				j++;
-				newstr[j] = '<';
-				j++;
-				newstr[j] = '<';
-				j++;
-				newstr[j] = ' ';
-				i += 1;
-			}
-		}
-		else
-			newstr[j] = data->prompt[i];
-		j++;
-		i++;
-	}
-	return (newstr);
 }
 
 //Count the number of redirection (times 2) for ft_expend
